29d5d733e7c1a989dfbc55ce7fee3364a92b3962
getrasa <getrasa4@gmail.com>
2023-05-11 07:40:23 +1000

    Improved whitespace remover

--- src/modules/hooks/processAttachedFiles.ts
+++ src/modules/hooks/processAttachedFiles.ts
@@ -1,14 +1,18 @@
 import { InlineFile } from "../components/FileAttacher";
 
-
 export const removeWhiteSpace = (inputString: string): string => {
-    return inputString.replace(/\s+/g, "");
-  }
+  return inputString.replace(/\s+/g, "");
+};
+
+function removeExtraWhitespace(input: string): string {
+  return input.replace(/\s+/g, " ").trim();
+}
 
 export const minimiazeAttachedFiles = (files: InlineFile[]): string => {
-    return files.reduce((acc, file) => {
-      return (
-        acc + ` ${"\n" + file.name + "\n```" + removeWhiteSpace(file.content) + "```"}`
-      );
-    }, "");
-  };
+  return files.reduce((acc, file) => {
+    return (
+      acc +
+      ` ${"\n" + file.name + "\n```" + removeExtraWhitespace(file.content) + "```"}`
+    );
+  }, "");
+};
